f5894ed0d38e180ea70d6b972f72236220793bbf
Mauricio Oleaga <maoa.develoment@gmail.com>
2022-06-17 11:00:41 -0400

    Perf: submitHandler was perfected
    In the submitHandler in the component MovieDetail was implement a method trim and was added a new swAlert

--- src/components/Searcher.js
+++ src/components/Searcher.js
@@ -8,7 +8,8 @@ const Searcher = () => {
     const submitHandler = e => {
         e.preventDefault();
 
-        const keyword = e.currentTarget.keyword.value;
+        // el "trim" es un metodo que quita los espacios en blanco antes de la 1ra palabra y despues de la ultima dentro de un string
+        const keyword = e.currentTarget.keyword.value.trim();
         // para acceder al valor del input con "currentTarget" se accede al input en general, al indicarle "keyword" que es el name del input
         // podremos tener acceso a la propiedad del input, el cual nos interesa sera el value
 
@@ -16,8 +17,14 @@ const Searcher = () => {
             swAlert({
                 title: "You should type a keyword at least",
                 icon: "warning"
+            });
+        } else if (keyword.length < 4) {
+            swAlert({
+                title: "You should type 4 letters at least",
+                icon: "warning"
             })
         }
+
     }
 
     return (
