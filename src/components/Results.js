58be641afdbf4d22f6a7d803e573d75e721761d5
Mauricio Oleaga <maoa.develoment@gmail.com>
2022-06-22 12:38:35 -0400

    Feat: setting endpoint and adding some hmtl
    It was implemented  a logic to get the params from the URL and with that inside a useEffect was set an endpoint to request API the movies. Also was added some html and style to make the component looks good.

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,16 +1,76 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link, useSearchParams } from 'react-router-dom';
+import swAlert from '@sweetalert/with-react';
+
 
 const Results = () => {
 
-    const query = new URLSearchParams(window.location.search);
-    const keyword = query.get('keyword');
+    let [searchParams] = useSearchParams();
+    // usamos en este component useSearchParams porque con URLSearchParams(window.location.search); pero este hace lo mismo que el anterior metodo 
+    const keyword = searchParams.get('keyword');
+    // se crea una var igaul y con el method get se coloca el nombre del key y quedara almacenado en la var
+    
+    const [moviesResults, setMoviesResults] = useState([]);
+    
+    
+    useEffect(() => {
 
-  return (
-    <div className="container">
-        <h2>Resultados</h2>
-        <p>Buscando a : {keyword}</p>
-    </div>
-  )
+
+        const endPoint = `https://api.themoviedb.org/3/search/movie?api_key=e89813675e342efb3edc61f9269a4f1a&language=en-US&query=${keyword}`;
+
+        // endpoint para hacer el request al API del get de las movies search
+        axios.get(endPoint)
+            .then(res => {
+                const moviesArray = res.data.results;
+                // actualizamos el state con la data que trae el endpoint
+
+                if (moviesArray.length === 0) {
+                    swAlert({
+                        title: 'No se encontro ninguna pelicula con este nombre',
+                        icon: "warning"
+                    })
+                }
+                setMoviesResults(moviesArray)
+            })
+            .catch(error => {
+                console.warn(error);
+            })
+        
+    }, [keyword])
+
+    return (
+        <div className="container">
+            <h2>Buscaste : <em> {keyword} </em> </h2>
+
+            {/* circuit cut render para que cuando no halla resultados muestre algo */}
+            {moviesResults.length === 0 && <h3>No hay resultados</h3>}
+
+            <div className="content-wrap">
+                <div className="row">
+                    {moviesResults.map((movie, index) => {
+                        const { title, overview, poster_path, id } = movie;
+
+                        return (
+                            <div className="col-3" key={index}>
+                                <div className="card my-4">
+                                    <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{title.substring(0, 30)}...</h5>
+                                        {/* aca el substring lo que hace es recortar el texto y resumirlo a la cantidad de palabras que se desee */}
+                                        <p className="card-text">{overview.substring(0, 100)}...</p>
+                                        <Link to={`/movieDetail?movieID=${id}`} className="btn btn-primary">View detail</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Results;
