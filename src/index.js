8f5edfb1e22313c288439d78c9080a722c7e54f5
Mauricio Oleaga <maoa.develoment@gmail.com>
2022-11-24 15:11:17 -0400

    add basename in BrowserRouter in index.js file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename="/Billboard-Cinema--Alkemy">
 			{/* el BrowserRouter permite gestionar el browser page aplication permitiendo ingresar a una ruta o redireccionamiento */}
 			<App />
 		</BrowserRouter>
